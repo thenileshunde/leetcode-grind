2d27f384563a0afeb728de99e377f0350f5a6d52
Nilesh Unde <thenileshunde@gmail.com>
2026-01-10 00:38:22 +0530

    Leetcode-2656 added optimal logic removed 2nd while loop

--- Leetcode-2656-MaxSumWithExactlyKElements/Solution.java
+++ Leetcode-2656-MaxSumWithExactlyKElements/Solution.java
@@ -1,18 +1,28 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
-        int max = nums[0];
-        for(int i:nums)
-        {
-            max = Math.max(max, i);
-        }
+        // ****************** approach 1 Optimal **********************
+        // int max = nums[0];
+        // for(int i:nums)
+        // {
+        //     max = Math.max(max, i);
+        // }
         
-        int sum = 0;
-        while(--k>0)
+        // int sum = 0;
+        // while(k>0)
+        // {
+        //     sum += max;
+        //     max += 1;
+        //     k--;
+        // }
+        // return sum;
+
+        // ****************** approach 2 Optimal **********************
+        int max = nums[0];
+        for(int i=1; i<nums.length; i++)
         {
-            sum += max;
-            max += 1;
-            k--;
+            max = Math.max(max, nums[i]);
         }
-        return sum;
+        int ans = (max * k + (k-1)*k/2);
+        return ans;
     }
 }
