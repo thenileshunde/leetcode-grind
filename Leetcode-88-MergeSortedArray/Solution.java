864e69688f0c3fb26c19044dcb00d343ff16a0ed
Nilesh Unde <thenileshunde@gmail.com>
2026-01-18 21:56:23 +0530

    Leetcode-0977 : Solved without new variables

--- Leetcode-88-MergeSortedArray/Solution.java
+++ Leetcode-88-MergeSortedArray/Solution.java
@@ -1,25 +1,29 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n == 0) return;
-        if(m == 0) 
+        // ****************** approach 1 **********************
+        // int i = m-1, j=n-1,  k = m+n-1;
+        // while(j>=0)
+        // {
+        //     if(i>=0 && nums1[i]>=nums2[j])
+        //     {
+        //         nums1[k--] = nums1[i--];
+        //     }
+        //     else
+        //     {
+        //         nums1[k--] = nums2[j--];
+        //     }
+        // }
+
+        // ****************** approach 2 - without using new variables **********************
+        while(n-1>=0)
         {
-            while(n != 0)
+            if(m-1>=0 && nums1[m-1]>=nums2[n-1])
             {
-                nums1[n-1] = nums2[n-1];
-                n--;
-            }
-            return;
-        }
-        while(n != 0)
-        {
-            if(m==0 || nums2[n-1]>=nums1[m-1])
-            {
-                nums1[m+n-1] = nums2[n-1]; n--;
+                nums1[m+n-1] = nums1[m-1]; m--;
             }
             else
             {
-                nums1[m+n-1] = nums1[m-1];
-                m--;
+                nums1[m+n-1] = nums2[n-1]; n--;
             }
         }
     }
